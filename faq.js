5fd52e0f71cf1718410ef1f91664142c1dcb3ae9
JuanAntonioLeonOjeda <jalojr@hotmail.com>
2025-02-18 16:31:19 +0000

    .env added

---
+++ faq.js
@@ -0,0 +1,12 @@
+const faq = {
+  testOne: {
+    description: "This is test one",
+    id: "f4df0165a54b486a9493145aed4a594c",
+  },
+  testTwo: {
+    description: "This is test two",
+    id: "cc572ead54ed4af3975f03afe598bb47",
+  },
+};
+
+module.exports = faq
